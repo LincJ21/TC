e89107f09f2a56d0dc4ec716a726291a49b5ff84
Carlos David Rodríguez González <142436671+Cardavidev@users.noreply.github.com>
2023-11-05 12:54:25 -0500

    Update: Help in navbar & help changes

--- app.py
+++ app.py
@@ -238,5 +238,9 @@ def about_Josue(request: Request):
 def about_Josue(request: Request):
     return templates.TemplateResponse('about_Carlos.html', {"request": request})
 
+@app.get("/help", response_class=HTMLResponse)
+def help(request: Request):
+    return templates.TemplateResponse('help.html', {"request": request})
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
