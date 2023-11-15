fd8aa4be8319703739fc9cbdd8e337ae61525694
Carlos David Rodríguez González <142436671+Cardavidev@users.noreply.github.com>
2023-11-15 17:34:07 -0500

    Uputudate
    XD

--- app.py
+++ app.py
@@ -173,7 +173,9 @@ def internet(request: Request):
 @app.get("/cobertura", response_class=HTMLResponse)
 def cobertura(request: Request):
     return templates.TemplateResponse('cobertura.html', {"request": request})
-
+@app.get("/familiar", response_class=HTMLResponse)
+def familiar(request: Request):
+    return templates.TemplateResponse('familiar.html', {"request": request})
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
